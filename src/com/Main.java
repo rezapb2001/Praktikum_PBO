598d54282b9f5720649135920435feadd5400132
rezapb2001 <rezapb2001@gmail.com>
2021-04-08 21:38:18 +0700

    Tugas 2 Exception

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,12 +1,30 @@
 package com;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Kubus kubus = new Kubus(2, "coklat");
 
-        kubus.berwarna();
-        kubus.setNamakubus("Bagus");
-        System.out.println("Nama Kubus : " + kubus.getNamakubus());
-        System.out.println("Luas Sisi Kubus : " + kubus.luas(4));
+        Scanner input = new Scanner(System.in);
+        int nilai = 0;
+
+        try {
+            System.out.println(" Masukan Nilai Untuk Panjang Sisi : ");
+            nilai = input.nextInt();
+
+        } catch (InputMismatchException error) {
+            System.out.println(" Input Anda Salah  yang anda masukan adalah string masukan intiger atau angka"
+                    + error.getMessage());
+
+        } finally {
+            Kubus kubus = new Kubus(nilai, "coklat");
+            System.out.println(" Panjang sisi kubus : " + nilai);
+            kubus.berwarna();
+            kubus.setNamakubus("Bagus");
+            System.out.println("Nama Kubus : " + kubus.getNamakubus());
+            System.out.println("Luas Sisi Kubus : " + kubus.luas());
+        }
+
     }
 }
